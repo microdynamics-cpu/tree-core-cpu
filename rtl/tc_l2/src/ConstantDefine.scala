9d07c1bc5b0c624f61b2b3eaca87b3b776c67b67
maksyuki <maksyuki@126.com>
2021-07-29 10:14:24 +0800

    chore: add in/out suffix to the io signals

--- rtl/tc_l2/src/ConstantDefine.scala
+++ rtl/tc_l2/src/ConstantDefine.scala
@@ -4,7 +4,9 @@ import chisel3._
 
 // len: the bits number
 trait ConstantDefine {
-  val BusWidth   = 32
-  val RegAddrLen = 5
-  val RegNum     = 32
+  val BusWidth     = 32
+  val RegAddrLen   = 5
+  val RegNum       = 32
+  val ALUOpcodeLen = 7
+  val ALUSelLen    = 3
 }
--- rtl/tc_l2/src/InstDecoderStage.scala
+++ rtl/tc_l2/src/InstDecoderStage.scala
@@ -4,10 +4,24 @@ import chisel3._
 
 class InstDecoderStage extends Module with ConstantDefine {
   val io = IO(new Bundle {
-    val ifInstAddr: UInt = Input(UInt(BusWidth.W))
-    val ifInstData: UInt = Input(UInt(BusWidth.W))
+    val instAddrIn: UInt = Input(UInt(BusWidth.W))
+    val instDataIn: UInt = Input(UInt(BusWidth.W))
 
-    val idInstAddr: UInt = Output(UInt(BusWidth.W))
-    val idInstData: UInt = Output(UInt(BusWidth.W))
+    val rdDataAIn: UInt = Input(UInt(BusWidth.W))
+    val rdDataBIn: UInt = Input(UInt(BusWidth.W))
+
+    val rdEnaAIn:  UInt = Output(Bool())
+    val rdAddrAIn: UInt = Output(UInt(RegAddrLen.W))
+    val rdEnaBIn:  UInt = Output(Bool())
+    val rdAddrBIn: UInt = Output(UInt(RegAddrLen.W))
+
+    val aluOpcodeOut: UInt = Output(UInt(ALUOpcodeLen.W))
+    val aluSelOut:    UInt = Output(UInt(ALUSelLen.W))
+    val rsValAOut:    UInt = Output(UInt(BusWidth.W))
+    val rsValBOut:    UInt = Output(UInt(BusWidth.W))
+
+    val wtEnaOut:  UInt = Output(Bool())
+    val wtAddrOut: UInt = Output(UInt(RegAddrLen.W))
   })
+
 }
