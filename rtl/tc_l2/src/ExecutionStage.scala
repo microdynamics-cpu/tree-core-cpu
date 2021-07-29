00e6f2ccb4da072b15ddf53d07928fd932165b22
maksyuki <maksyuki@126.com>
2021-07-29 18:58:34 +0800

    feat: add exec stage and op num type

--- rtl/tc_l2/src/ExecutionStage.scala
+++ rtl/tc_l2/src/ExecutionStage.scala
@@ -1,1 +1,16 @@
+package treecorel2
 
+import chisel3._
+import chisel3.util.MuxLookup
+
+object ExecutionStage {
+  val aluADDIType = 0.U(4.W)
+//   val ALU_SUB     = 1.U(4.W)
+//   val ALU_AND     = 2.U(4.W)
+//   val ALU_OR      = 3.U(4.W)
+  val aluNopType = 15.U(4.W)
+}
+
+// class ExecutionStage extends Module with ConstantDefine {
+
+// }
