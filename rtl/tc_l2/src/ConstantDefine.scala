1e32fa0e780aedf83a187fe2e43a8ecd69eed862
maksyuki <maksyuki@126.com>
2021-07-30 19:38:01 +0800

    feat: add icache volume define

--- rtl/tc_l2/src/ConstantDefine.scala
+++ rtl/tc_l2/src/ConstantDefine.scala
@@ -4,10 +4,11 @@ import chisel3._
 
 // len: the bits number
 trait ConstantDefine {
-  val BusWidth       = 32
+  val BusWidth     = 32
+  val InstCacheLen = 128
+
   val RegAddrLen     = 5
   val RegNum         = 32
   val ALUOperTypeLen = 4
-  val ALUSelLen      = 3
   val InstTypeLen    = 3
 }
