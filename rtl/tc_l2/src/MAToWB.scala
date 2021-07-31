d0febd84e53626436880a4361cdece0c5c7d5fba
maksyuki <maksyuki@126.com>
2021-07-31 08:47:36 +0800

    feat: add ma to wb module

---
+++ rtl/tc_l2/src/MAToWB.scala
@@ -0,0 +1,27 @@
+package treecorel2
+
+import chisel3._
+
+class MAToWB extends Module with ConstantDefine {
+  val io = IO(new Bundle {
+    val maResIn:    UInt = Input(UInt(BusWidth.W))
+    val maWtEnaIn:  Bool = Input(Bool())
+    val maWtAddrIn: UInt = Input(UInt(RegAddrLen.W))
+
+    val wbResOut:    UInt = Output(UInt(BusWidth.W))
+    val wbWtEnaOut:  Bool = Output(Bool())
+    val wbWtAddrOut: UInt = Output(UInt(RegAddrLen.W))
+  })
+
+  private val resRegister:    UInt = RegInit(0.U(BusWidth.W))
+  private val wtEnaRegister:  Bool = RegInit(false.B)
+  private val wtAddrRegister: UInt = RegInit(0.U(RegAddrLen.W))
+
+  resRegister    := Mux(this.reset.asBool(), 0.U(BusWidth.W), io.maResIn)
+  wtEnaRegister  := Mux(this.reset.asBool(), false.B, io.maWtEnaIn)
+  wtAddrRegister := Mux(this.reset.asBool(), 0.U(RegAddrLen.W), io.maWtAddrIn)
+
+  io.wbResOut    := resRegister
+  io.wbWtEnaOut  := wtEnaRegister
+  io.wbWtAddrOut := wtAddrRegister
+}
