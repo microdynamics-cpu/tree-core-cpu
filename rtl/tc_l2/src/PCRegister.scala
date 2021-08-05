0bc206c876daa5bdc15ffc9170ada8da0cb459bc
maksyuki <maksyuki@126.com>
2021-08-05 20:33:34 +0800

    feat: change the inst's width and pc's init addr

--- rtl/tc_l2/src/PCRegister.scala
+++ rtl/tc_l2/src/PCRegister.scala
@@ -8,15 +8,8 @@ class PCRegister extends Module with ConstantDefine {
     val instEnaOut:  Bool = Output(Bool())
   })
 
-  protected val pc: UInt = RegInit(0.U(BusWidth.W))
+  protected val pc: UInt = RegInit(PcRegStartAddr.U(BusWidth.W))
   pc             := pc + 4.U
   io.instAddrOut := pc
   io.instEnaOut  := !this.reset.asBool()
 }
-
-// object PCRegister extends App {
-//   (new chisel3.stage.ChiselStage).execute(
-//     args,
-//     Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new PCRegister()))
-//   )
-// }
