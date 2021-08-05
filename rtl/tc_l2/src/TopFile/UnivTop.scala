346e721db004b2f000989a35b47e793e9f9b9dab
maksyuki <maksyuki@126.com>
2021-08-05 21:33:40 +0800

    chore: add and modify universal and difftest build rule

--- rtl/tc_l2/src/TopFile/UnivTop.scala
+++ rtl/tc_l2/src/TopFile/UnivTop.scala
@@ -1,6 +1,6 @@
 import treecorel2._
 
-object Top extends App {
+object UnivTop extends App {
   (new chisel3.stage.ChiselStage).execute(
     args,
     Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new TreeCoreL2()))
