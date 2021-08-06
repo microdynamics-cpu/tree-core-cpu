7c59f4f295c4a1066e5c88ffaa0ff4975ad2d0d1
maksyuki <maksyuki@126.com>
2021-08-06 20:20:15 +0800

    chore: change the cp source dir

--- rtl/tc_l2/copy.sh
+++ rtl/tc_l2/copy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cp -r `ls | grep -v copy.sh | xargs` ../../../oscpu-dev-record/cpu
+# cp -r `ls | grep -v copy.sh | xargs` ../../../oscpu-dev-record/cpu
+cp -r src ../../../oscpu-dev-record/cpu
+cp -r test ../../../oscpu-dev-record/cpu
