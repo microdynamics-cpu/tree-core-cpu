179eedc6391fae0f7f2a0f10c7112033118e16a5
maksyuki <maksyuki@126.com>
2021-08-11 12:25:50 +0800

    style: change suffix 'Res' to 'Data'

--- rtl/tc_l2/src/main/scala/core/EXToMA.scala
+++ rtl/tc_l2/src/main/scala/core/EXToMA.scala
@@ -4,11 +4,11 @@ import chisel3._
 
 class EXToMA extends Module with ConstantDefine {
   val io = IO(new Bundle {
-    val exResIn:    UInt = Input(UInt(BusWidth.W))
+    val exDataIn:   UInt = Input(UInt(BusWidth.W))
     val exWtEnaIn:  Bool = Input(Bool())
     val exWtAddrIn: UInt = Input(UInt(RegAddrLen.W))
 
-    val maResOut:    UInt = Output(UInt(BusWidth.W))
+    val maDataOut:   UInt = Output(UInt(BusWidth.W))
     val maWtEnaOut:  Bool = Output(Bool())
     val maWtAddrOut: UInt = Output(UInt(RegAddrLen.W))
   })
@@ -17,11 +17,11 @@ class EXToMA extends Module with ConstantDefine {
   protected val wtEnaReg:  Bool = RegInit(false.B)
   protected val wtAddrReg: UInt = RegInit(0.U(RegAddrLen.W))
 
-  resReg    := io.exResIn
+  resReg    := io.exDataIn
   wtEnaReg  := io.exWtEnaIn
   wtAddrReg := io.exWtAddrIn
 
-  io.maResOut    := resReg
+  io.maDataOut   := resReg
   io.maWtEnaOut  := wtEnaReg
   io.maWtAddrOut := wtAddrReg
 }
--- rtl/tc_l2/src/main/scala/core/MAToWB.scala
+++ rtl/tc_l2/src/main/scala/core/MAToWB.scala
@@ -4,11 +4,11 @@ import chisel3._
 
 class MAToWB extends Module with ConstantDefine {
   val io = IO(new Bundle {
-    val maResIn:    UInt = Input(UInt(BusWidth.W))
+    val maDataIn:   UInt = Input(UInt(BusWidth.W))
     val maWtEnaIn:  Bool = Input(Bool())
     val maWtAddrIn: UInt = Input(UInt(RegAddrLen.W))
 
-    val wbResOut:    UInt = Output(UInt(BusWidth.W))
+    val wbDataOut:   UInt = Output(UInt(BusWidth.W))
     val wbWtEnaOut:  Bool = Output(Bool())
     val wbWtAddrOut: UInt = Output(UInt(RegAddrLen.W))
   })
@@ -17,15 +17,15 @@ class MAToWB extends Module with ConstantDefine {
   protected val wtEnaReg:  Bool = RegInit(false.B)
   protected val wtAddrReg: UInt = RegInit(0.U(RegAddrLen.W))
 
-  resReg    := io.maResIn
+  resReg    := io.maDataIn
   wtEnaReg  := io.maWtEnaIn
   wtAddrReg := io.maWtAddrIn
 
-  io.wbResOut    := resReg
+  io.wbDataOut   := resReg
   io.wbWtEnaOut  := wtEnaReg
   io.wbWtAddrOut := wtAddrReg
 
-  //@printf(p"[ma2wb]io.wbResOut = 0x${Hexadecimal(io.wbResOut)}\n")
+  //@printf(p"[ma2wb]io.wbDataOut = 0x${Hexadecimal(io.wbDataOut)}\n")
   //@printf(p"[ma2wb]io.wbWtEnaOut = 0x${Hexadecimal(io.wbWtEnaOut)}\n")
   //@printf(p"[ma2wb]io.wbWtAddrOut = 0x${Hexadecimal(io.wbWtAddrOut)}\n")
 }
