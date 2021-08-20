1eaf4aab69858463174d60cb34aff71520ad7235
maksyuki <maksyuki@126.com>
2021-08-20 20:41:02 +0800

    feat: add mcycle reg in csr

--- rtl/tc_l2/src/main/scala/core/CSRReg.scala
+++ rtl/tc_l2/src/main/scala/core/CSRReg.scala
@@ -3,5 +3,10 @@ package treecorel2
 import chisel3._
 
 class CSRReg extends Module with InstConfig {
-  val io = IO(new Bundle {})
+  val io = IO(new Bundle {
+    val cycleOut: UInt = Output(UInt(BusWidth.W))
+  })
+
+  protected val cycleReg: UInt = RegInit(0.U(BusWidth.W))
+  cycleReg := cycleReg + 1.U
 }
