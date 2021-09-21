aa022c84bdbdffe6f731e75eaa1a3e82d2cecb3e
maksyuki <maksyuki@126.com>
2021-09-21 14:58:59 +0800

    feat: add mulit csr reg read oper

--- rtl/tc_l2/src/main/scala/core/CSRReg.scala
+++ rtl/tc_l2/src/main/scala/core/CSRReg.scala
@@ -10,8 +10,9 @@ class CSRReg(val ifDiffTest: Boolean) extends Module with InstConfig {
     // from id
     val rdAddrIn: UInt = Input(UInt(CSRAddrLen.W))
     // from ex's out
-    val wtEnaIn:  Bool = Input(Bool())
-    val wtDataIn: UInt = Input(UInt(BusWidth.W))
+    val wtEnaIn:      Bool = Input(Bool())
+    val wtInstTypeIn: UInt = Input(UInt(InstOperTypeLen.W))
+    val wtDataIn:     UInt = Input(UInt(BusWidth.W))
     // to ex's in
     val rdDataOut: UInt = Output(UInt(BusWidth.W))
     // to difftest
@@ -40,7 +41,14 @@ class CSRReg(val ifDiffTest: Boolean) extends Module with InstConfig {
     io.rdAddrIn,
     0.U(BusWidth.W),
     Seq(
-      mCycleAddr -> mcycle
+      mStatusAddr -> mstatus,
+      mIeAddr     -> mie,
+      mTvecAddr   -> mtvec,
+      mEpcAddr    -> mepc,
+      mCauseAddr  -> mcause,
+      mTvalAddr   -> mtval,
+      mIpAddr     -> mip,
+      mCycleAddr  -> mcycle
     )
   )
 
@@ -51,6 +59,14 @@ class CSRReg(val ifDiffTest: Boolean) extends Module with InstConfig {
   }
 
   if (ifDiffTest) {
+    val diffArchState = Module(new DifftestArchEvent())
+    diffArchState.io.clock       := this.clock
+    diffArchState.io.coreid      := 0.U
+    diffArchState.io.intrNO      := 0.U
+    diffArchState.io.cause       := 0.U
+    diffArchState.io.exceptionPC := 0.U
+    // diffArchState.io.exceptionInst := 0.U // FIXME: current version don't have this api, maybe need to update the difftest
+
     val diffCsrState = Module(new DifftestCSRState())
     diffCsrState.io.clock          := this.clock
     diffCsrState.io.coreid         := 0.U
