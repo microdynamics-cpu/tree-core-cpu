0a6b58346051f5ce7e6c0b8d59918dad8d91b91f
maksyuki <maksyuki@126.com>
2021-09-28 14:56:29 +0800

    feat: add soc top obj to gen ysyx format file

--- rtl/tc_l2/src/main/scala/top/TopMain.scala
+++ rtl/tc_l2/src/main/scala/top/TopMain.scala
@@ -3,21 +3,20 @@ package treecorel2
 import treecorel2.common.ConstVal
 
 object TopMain extends App {
-  if (ConstVal.DiffTestEna) {
-    (new chisel3.stage.ChiselStage).execute(
-      args,
-      Seq(
-        chisel3.stage.ChiselGeneratorAnnotation(() => new SimTop(true))
-      )
+  (new chisel3.stage.ChiselStage).execute(
+    args,
+    Seq(
+      chisel3.stage.ChiselGeneratorAnnotation(() => new SimTop(true))
     )
-  } else {
-    (new chisel3.stage.ChiselStage).execute(
-      args,
-      Seq(
-        chisel3.stage.ChiselGeneratorAnnotation(() => new SimTop(false)),
-        firrtl.stage.RunFirrtlTransformAnnotation(new AddModulePrefix()),
-        ModulePrefixAnnotation("ysyx_210324_")
-      )
+  )
+
+  (new chisel3.stage.ChiselStage).execute(
+    args,
+    Seq(
+      chisel3.stage.ChiselGeneratorAnnotation(() => new SoCTop(false)),
+      firrtl.stage.RunFirrtlTransformAnnotation(new AddModulePrefix()),
+      ModulePrefixAnnotation("ysyx_210324_")
     )
-  }
+  )
+
 }
