2ddf3b9b5aa4b420053df823642a56a0a8b51931
maksyuki <maksyuki@126.com>
2022-01-01 15:29:37 +0800

    feat: add top main file

---
+++ rtl/tc_l2/src/main/scala/top/TopMain.scala
@@ -0,0 +1,23 @@
+package treecorel2
+
+import treecorel2.common.ConstVal
+
+object TopMain extends App with InstConfig {
+  if (DiffEn) {
+    (new chisel3.stage.ChiselStage).execute(
+      args,
+      Seq(
+        chisel3.stage.ChiselGeneratorAnnotation(() => new SimTop())
+      )
+    )
+  } else {
+    (new chisel3.stage.ChiselStage).execute(
+      args,
+      Seq(
+        chisel3.stage.ChiselGeneratorAnnotation(() => new SoCTop()),
+        firrtl.stage.RunFirrtlTransformAnnotation(new AddModulePrefix()),
+        ModulePrefixAnnotation("ysyx_210324_")
+      )
+    )
+  }
+}
