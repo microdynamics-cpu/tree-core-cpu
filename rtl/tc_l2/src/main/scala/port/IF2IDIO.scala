6cfd2409c884dad4cb8560a28a5eb8adc75e90ae
maksyuki <maksyuki@126.com>
2022-01-07 16:01:17 +0800

    feat: add needed io port def for branch pred

--- rtl/tc_l2/src/main/scala/port/IF2IDIO.scala
+++ rtl/tc_l2/src/main/scala/port/IF2IDIO.scala
@@ -3,8 +3,12 @@ package treecorel2
 import chisel3._
 import chisel3.util._
 
+import treecorel2.common.ConstVal
+
 class IF2IDIO extends Bundle {
-  val valid = Output(Bool())
-  val inst  = Output(UInt(32.W))
-  val pc    = Output(UInt(64.W))
+  val valid     = Output(Bool())
+  val inst      = Output(UInt(32.W))
+  val pc        = Output(UInt(64.W))
+  val branIdx   = Output(UInt(ConstVal.GHRLen.W))
+  val predTaken = Output(Bool())
 }
