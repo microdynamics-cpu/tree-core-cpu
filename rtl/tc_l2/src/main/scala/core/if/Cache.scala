bc49030b09ff0620e72d9aa4034d2cbb5ae5d94a
maksyuki <maksyuki@126.com>
2022-01-12 19:17:21 +0800

    feat: add cache req and resp io define

--- rtl/tc_l2/src/main/scala/core/if/Cache.scala
+++ rtl/tc_l2/src/main/scala/core/if/Cache.scala
@@ -3,6 +3,22 @@ package treecorel2
 import chisel3._
 import chisel3.util._
 
+
+class CacheReqIO extends Bundle {
+  //
+  //    |--------- tag ---------|---index---|--offset--|
+  //
+  val addr = UInt(64.W)
+  val data = UInt(64.W)
+  val mask = UInt((64/8).W)
+  val op = UInt(1.W)  // 0: rd   1: wr
+}
+
+class CacheRespIO extends Bundle{
+  val data = UInt(64.W)
+  val cmd = UInt(4.W)
+}
+
 class Cache extends Module {
     
 }
