a8f30f8d62f23b2083967f051b7f1a6e7caad799
maksyuki <maksyuki@126.com>
2022-01-15 13:31:57 +0800

    feat: add cache core io define

--- rtl/tc_l2/src/main/scala/common/InstConfig.scala
+++ rtl/tc_l2/src/main/scala/common/InstConfig.scala
@@ -5,4 +5,12 @@ import chisel3.util._
 
 trait InstConfig {
   val SoCEna = false
+
+  val XLen          = 64
+  val NWay          = 4
+  val NBank         = 4
+  val CacheLineSize = 64 * NBank
+  val CacheLineLen  = 32
+  val ICacheSize    = NWay * CacheLineSize * CacheLineLen
+  val DCacheSize    = NWay * CacheLineSize * CacheLineLen
 }
--- rtl/tc_l2/src/main/scala/core/if/Cache.scala
+++ rtl/tc_l2/src/main/scala/core/if/Cache.scala
@@ -1,4 +1,23 @@
-// package treecorel2
+package treecorel2
 
-// import chisel3._
-// import chisel3.util._
+import chisel3._
+import chisel3.util._
+
+import treecorel2.common.InstConfig
+
+class CacheReqIO extends Bundle with InstConfig {
+  val addr = UInt(XLen.W)
+  val data = UInt(XLen.W) // for write
+  val mask = UInt((XLen / 8).W) // for write
+  val op   = UInt(1.W)
+}
+
+class CacheRespIO extends Bundle with InstConfig {
+  val cmd  = UInt(4.W)
+  val data = UInt(XLen.W)
+}
+
+class CacheCoreIO extends Bundle {
+  val req  = Flipped(Decoupled(new CacheReqIO))
+  val resp = Valid(new CacheRespIO)
+}
