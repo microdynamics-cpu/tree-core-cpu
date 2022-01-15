ec9a68802d44fe95126784b2d637d03d44ff4fdd
maksyuki <maksyuki@126.com>
2022-01-15 20:46:15 +0800

    feat: modify cache config

--- rtl/tc_l2/src/main/scala/common/InstConfig.scala
+++ rtl/tc_l2/src/main/scala/common/InstConfig.scala
@@ -4,13 +4,13 @@ import chisel3._
 import chisel3.util._
 
 trait InstConfig {
-  val SoCEna = false
-
+  val SoCEna        = false
+  val CacheEna      = false
   val XLen          = 64
   val NWay          = 4
   val NBank         = 4
+  val NSet          = 32
   val CacheLineSize = 64 * NBank
-  val CacheLineLen  = 32
-  val ICacheSize    = NWay * CacheLineSize * CacheLineLen
-  val DCacheSize    = NWay * CacheLineSize * CacheLineLen
+  val ICacheSize    = NWay * NSet * CacheLineSize
+  val DCacheSize    = NWay * NSet * CacheLineSize
 }
--- rtl/tc_l2/src/main/scala/core/if/Cache.scala
+++ rtl/tc_l2/src/main/scala/core/if/Cache.scala
@@ -9,15 +9,41 @@ class CacheReqIO extends Bundle with InstConfig {
   val addr = UInt(XLen.W)
   val data = UInt(XLen.W) // for write
   val mask = UInt((XLen / 8).W) // for write
-  val op   = UInt(1.W)
 }
 
 class CacheRespIO extends Bundle with InstConfig {
-  val cmd  = UInt(4.W)
   val data = UInt(XLen.W)
 }
 
 class CacheCoreIO extends Bundle {
-  val req  = Flipped(Decoupled(new CacheReqIO))
+  val abort = UInt(Bool())
+  val req  = Flipped(Valid(new CacheReqIO))
   val resp = Valid(new CacheRespIO)
+}
+
+class CacheMemReqIO extends Bundle with InstConfig {
+  val id   = UInt(4.W)
+  val cmd  = UInt(4.W)
+  val addr = UInt(XLen.W)
+  val data = UInt(XLen.W)
+  val len  = UInt(2.W)
+}
+
+class CacheMemRespIO extends Bundle with InstConfig {
+  val id   = UInt(4.W)
+  val cmd  = UInt(4.W)
+  val data = UInt(XLen.W)
+
+}
+
+class CacheMemIO extends Bundle {
+  val req  = Decoupled(new CacheMemReqIO)
+  val resp = Flipped(Decoupled(new CacheMemRespIO))
+}
+
+class Cache(val cacheType: String) extends Module with InstConfig {
+  val io = IO(new Bundle {
+    val core = new CacheCoreIO
+    val mem  = new CacheMemIO
+  })
 }
